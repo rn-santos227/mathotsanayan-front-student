c89e7bb801b299c0475e6992f23ea7d5427ab36b
rn-santos227 <rn_santos227@yahoo.com>
2024-01-01 21:52:05 +0800

    added help dialog

---
+++ src/views/help/DialogView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn density="comfortable" icon="mdi-help-circle-outline" size="x-large">
+    <v-dialog
+      class="ma-auto"
+      persistent
+      v-model="dialog"
+      activator="parent"
+      width="70%"
+    >
+    </v-dialog>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const dialog = ref<boolean>(false);
+</script>

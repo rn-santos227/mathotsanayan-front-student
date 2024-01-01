eb07502ccf8a1791e491f73e69160ea897eca67a
rn-santos227 <rn_santos227@yahoo.com>
2024-01-01 23:36:35 +0800

    fixed help dialog button popup

--- src/views/help/DialogView.vue
+++ src/views/help/DialogView.vue
@@ -1,6 +1,16 @@
 <template>
-  <v-btn density="comfortable" icon="mdi-help-circle-outline" size="x-large">
-  </v-btn>
+  <v-btn
+    density="comfortable"
+    icon="mdi-help-circle-outline"
+    size="x-large"
+    @click="dialog = true"
+  />
+
+  <v-dialog v-model="dialog" width="auto"> </v-dialog>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const dialog = ref<boolean>(false);
+</script>

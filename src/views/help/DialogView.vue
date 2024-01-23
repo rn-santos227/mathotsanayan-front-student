aa3a625390d869b33a32f945c48fbe8b8b383ff6
rn-santos227 <rn_santos227@yahoo.com>
2024-01-23 22:19:11 +0800

    upadte help library

--- src/views/help/DialogView.vue
+++ src/views/help/DialogView.vue
@@ -6,11 +6,30 @@
     @click="dialog = true"
   />
 
-  <v-dialog v-model="dialog" width="auto"> </v-dialog>
+  <v-dialog class="ma-auto" width="70%" persistent v-model="dialog">
+    <v-card>
+      <v-card class="rounded-0 rounded-t py-2" color="purple-darken-3" flat>
+        <v-card-actions class="mx-4">
+          <span class="text-h6"> Help Library </span>
+          <v-spacer />
+          <v-btn
+            density="comfortable"
+            variant="outlined"
+            icon="mdi-close"
+            @click="close"
+          >
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
 const dialog = ref<boolean>(false);
+const close = () => {
+  dialog.value = !dialog.value;
+};
 </script>

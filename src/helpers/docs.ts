a0f677fb0af39dbf99a47bd50ce3a03deaa8ba9f
rn-santos227 <rn_santos227@yahoo.com>
2024-01-26 21:15:22 +0800

    added docs list

---
+++ src/helpers/docs.ts
@@ -0,0 +1,35 @@
+export default [
+  {
+    text: "Introduction",
+    page: "intro",
+    subpages: [],
+  },
+  {
+    text: "Profile",
+    page: "profile",
+    subpages: [],
+  },
+  {
+    text: "Modules",
+    page: "module",
+    subpages: [],
+  },
+  {
+    text: "Progress",
+    page: "progress",
+    subpages: [
+      {
+        text: "Progress",
+        page: "progress-main",
+      },
+      {
+        text: "Results Details",
+        page: "reports-details",
+      },
+      {
+        text: "Answers List",
+        page: "reports-answers",
+      },
+    ],
+  },
+];

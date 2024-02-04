a01430b15b403b8be2ca088e64f8a4f470e47a35
rn-santos227 <rn_santos227@yahoo.com>
2024-02-04 22:27:50 +0800

    update dashboard components and module

--- src/store/modules/dashboard_module.ts
+++ src/store/modules/dashboard_module.ts
@@ -1,9 +1,33 @@
 import Dashboard from "@/interfaces/Dashboard";
+import GraphPie from "@/interfaces/GraphPie";
+import ResultModule from "@/interfaces/ResultModule";
 import { defineStore } from "pinia";
 
 export const useDashboardModule = defineStore("dashboard", {
   state: () => ({
     dashboard: {} as Dashboard,
     isLoading: false,
+    graphPie: {} as GraphPie,
+    resultModules: [] as ResultModule[],
   }),
+
+  actions: {
+    setDashboard(dashboard: Dashboard): void {
+      this.dashboard = dashboard;
+    },
+
+    setGraphPie(graphPie: GraphPie): void {
+      this.graphPie = graphPie;
+    },
+
+    setResultModules(resultModules: ResultModule[]) {
+      this.resultModules = resultModules;
+    },
+  },
+
+  getters: {
+    getDashboard(): Dashboard {
+      return this.dashboard;
+    },
+  },
 });

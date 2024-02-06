5f617a7705ed60eb61228c89ea65083f75d9f230
rn-santos227 <rn_santos227@yahoo.com>
2024-02-06 21:47:33 +0800

    update dashboard in profile page

--- src/views/profile/DashboardView.vue
+++ src/views/profile/DashboardView.vue
@@ -3,3 +3,27 @@
     <div class="d-flex justify-space-around flex-wrap"></div>
   </v-container>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { useDashboardModule } from "@/store";
+
+import DashboardCardComponent from "@/components/dashboard/DashboardCardComponent.vue";
+import DashboardCard from "@/interfaces/DashboardCard";
+
+const moduleCard = ref<DashboardCard>({
+  count: useDashboardModule().getDashboard.modules,
+  title: "Total Modules",
+  link: "/modules",
+  color: "indigo",
+  icon: "mdi-text-box-check",
+});
+
+onMounted(async () => {
+  useDashboardModule().isLoading = true;
+  useDashboardModule().read();
+  moduleCard.value.count = useDashboardModule().getDashboard.modules;
+
+  useDashboardModule().isLoading = false;
+});
+</script>

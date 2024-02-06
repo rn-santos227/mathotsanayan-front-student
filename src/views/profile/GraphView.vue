0d1aa4a19003560249c8de8ced97e5a4c6681b9d
rn-santos227 <rn_santos227@yahoo.com>
2024-02-06 22:55:14 +0800

    added graph view in profile

---
+++ src/views/profile/GraphView.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container fluid>
+    <div class="d-flex justify-space-around flex-wrap"></div>
+  </v-container>
+</template>
+
+<script setup lang="ts"></script>
--- src/views/profile/MainView.vue
+++ src/views/profile/MainView.vue
@@ -2,10 +2,12 @@
   <v-container class="mt-8" fluid>
     <DashboardView />
     <ProfileView />
+    <GraphView />
   </v-container>
 </template>
 
 <script setup lang="ts">
 import ProfileView from "./ProfileView.vue";
 import DashboardView from "./DashboardView.vue";
+import GraphView from "./GraphView.vue";
 </script>

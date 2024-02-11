37109dc9f056145520456982c6b6ab01a2ae11ad
rn-santos227 <rn_santos227@yahoo.com>
2024-02-11 20:18:41 +0800

    update mobile view page for help panel

--- src/helpers/documentation/doc_mobile.ts
+++ src/helpers/documentation/doc_mobile.ts
@@ -1,3 +1,29 @@
-const docs: string[] = [``];
+const docs: string[] = [
+  `Experience the adaptive mobile view of MatHotSanayan's web application
+  with the Student Access feature, providing seamless access to the academic
+  profile on any mobile device through the browser. This adaptive design
+  ensures optimal viewing and interaction, allowing students to navigate the
+  platform with ease, whether accessing it from a smartphone or tablet. With
+  responsive layouts and intuitive controls, students can access their
+  personalized student profile, view exam results, and track their academic
+  progress conveniently from anywhere, at any time.`,
+
+  `The Student Access feature offers a user-friendly interface tailored for
+  mobile devices, ensuring that students can effortlessly engage with
+  MatHotSanayan's functionalities on the go. Whether accessing the platform
+  from a smartphone during a commute or from a tablet during a study
+  session, students can enjoy a consistent and intuitive user experience.
+  With adaptive layouts and optimized performance, MatHotSanayan's Student
+  Access feature empowers students to stay connected and informed about
+  their academic journey, regardless of their device or location.`,
+
+  `By providing adaptive mobile access to the web application through the
+  browser, MatHotSanayan ensures that students have the flexibility and
+  convenience to manage their academic profile anytime, anywhere. Whether
+  checking exam results, reviewing study materials, or accessing student
+  resources, students can enjoy a seamless experience across various mobile
+  devices, enhancing their productivity and enabling them to stay on top of
+  their studies effortlessly.`,
+];
 
 export default docs;
--- src/views/help/documentation/DocMobileView.vue
+++ src/views/help/documentation/DocMobileView.vue
@@ -4,5 +4,44 @@
       MatHotSanayan Student Mobile View
     </span>
     <v-divider class="my-2" />
+    <p class="ma-6">
+      {{ docs[0] }}
+    </p>
+
+    <v-row>
+      <v-col>
+        <v-img
+          class="ma-6"
+          cover
+          :src="require('@/assets/images/help/doc_003.png')"
+        />
+      </v-col>
+      <v-col>
+        <v-img
+          class="ma-6"
+          cover
+          :src="require('@/assets/images/help/doc_004.png')"
+        />
+      </v-col>
+      <v-col>
+        <v-img
+          class="ma-6"
+          cover
+          :src="require('@/assets/images/help/doc_005.png')"
+        />
+      </v-col>
+    </v-row>
+
+    <p class="ma-6">
+      {{ docs[1] }}
+    </p>
+
+    <p class="ma-6">
+      {{ docs[2] }}
+    </p>
   </v-card-text>
 </template>
+
+<script setup lang="ts">
+import docs from "@/helpers/documentation/doc_mobile";
+</script>

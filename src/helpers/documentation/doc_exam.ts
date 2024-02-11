9e4ce0a32c7ea85464e6bc9d9eb818049c3edbf4
rn-santos227 <rn_santos227@yahoo.com>
2024-02-11 22:14:12 +0800

    update exam page in help panel

--- src/helpers/documentation/doc_exam.ts
+++ src/helpers/documentation/doc_exam.ts
@@ -1,3 +1,44 @@
-const docs: string[] = [``];
+const docs: string[] = [
+  `Welcome to the Exam Page in MatHotSanayan, the platform designed to
+  facilitate assessment-taking and progress monitoring. This dynamic page
+  provides a user-friendly interface, guiding users through each step of the
+  examination process seamlessly.`,
+
+  `Upon entering the Exam Page, questions are presented one at a time,
+  allowing students to focus their attention and thoughtfully consider each
+  query. Navigation between questions is intuitive, either through the
+  provided navigation buttons or by selecting the question number from the
+  question panel. This structured approach ensures clarity and ease of use
+  as students progress through the assessment.`,
+
+  `As students encounter each question, they have the option to answer it or
+  defer if uncertain. This flexibility empowers individuals to approach the
+  exam at their own pace and strategically manage their responses. If a
+  student chooses to answer, they can select their response and move on to
+  the next question. Alternatively, if unsure about a question, it can be
+  skipped and returned to later for further consideration.`,
+
+  `MatHotSanayan recognizes the importance of learning from mistakes and
+  offers multiple attempts for answering each question. This feature
+  encourages a growth mindset, allowing students to refine their
+  understanding and problem-solving skills through iterative learning.
+  However, after three incorrect attempts, the system will automatically
+  mark the question as incorrect, promoting efficient progress through the
+  exam.`,
+
+  `Real-time feedback is integral to the exam-taking experience in
+  MatHotSanayan. Upon submitting an answer to a question, immediate feedback
+  is provided, indicating whether the response is correct or incorrect. This
+  instant validation allows students to monitor their performance in
+  real-time, enabling them to adjust their strategy and focus on areas that
+  require further attention.`,
+
+  `By leveraging the features available on the Exam Page effectively,
+  students can approach assessments with confidence, optimize their
+  exam-taking strategy, and ultimately achieve their academic goals. Whether
+  aiming to master course content, track progress, or prepare for upcoming
+  evaluations, MatHotSanayan's Exam Page provides the tools and support
+  needed for success.`,
+];
 
 export default docs;

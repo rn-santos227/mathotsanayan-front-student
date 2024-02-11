af236cbdb98ae145c2361ffe80bdaa7c65873626
rn-santos227 <rn_santos227@yahoo.com>
2024-02-11 21:49:43 +0800

    update mobile and module page for help module

--- src/helpers/documentation/doc_module.ts
+++ src/helpers/documentation/doc_module.ts
@@ -1,3 +1,29 @@
-const docs: string[] = [``];
+const docs: string[] = [
+  `Navigate through MatHotSanayan's Modules/Exams page to access available
+  assessments tailored to your academic progress and chosen subjects. This
+  intuitive feature enables students to select exams based on their current
+  stage of learning, ensuring that they encounter assessments relevant to
+  their level of proficiency. Before proceeding to take an exam, students
+  can review essential information such as the module or examination's
+  description, objective, and instructions, providing clarity and guidance
+  before beginning the assessment.`,
+
+  `Start by selecting the desired subject from the list of available options
+  on the Modules/Exams page. Each subject will display the corresponding
+  modules or examinations that are accessible to students at their current
+  stage of progress. This tailored approach ensures that students encounter
+  assessments aligned with their academic journey, promoting meaningful
+  learning experiences. Additionally, students can review detailed
+  descriptions and objectives for each module or examination, allowing them
+  to understand the content and objectives before proceeding further.`,
+
+  `Once students have chosen a module or examination, they can access
+  detailed instructions to guide them through the assessment process. These
+  instructions provide clarity on how to navigate the exam interface, answer
+  questions, and submit responses effectively. By providing comprehensive
+  information upfront, MatHotSanayan's Modules/Exams page empowers students
+  to approach assessments with confidence and clarity, ultimately enhancing
+  their learning outcomes and academic success.`,
+];
 
 export default docs;
--- src/views/help/documentation/DocModulesView.vue
+++ src/views/help/documentation/DocModulesView.vue
@@ -4,5 +4,32 @@
       MatHotSanayan Student Available Module
     </span>
     <v-divider class="my-2" />
+    <p class="ma-6">
+      {{ docs[0] }}
+    </p>
+
+    <v-img
+      class="ma-6"
+      cover
+      :src="require('@/assets/images/help/doc_006.png')"
+    />
+
+    <p class="ma-6">
+      {{ docs[1] }}
+    </p>
+
+    <v-img
+      class="ma-6"
+      cover
+      :src="require('@/assets/images/help/doc_007.png')"
+    />
+
+    <p class="ma-6">
+      {{ docs[2] }}
+    </p>
   </v-card-text>
 </template>
+
+<script setup lang="ts">
+import docs from "@/helpers/documentation/doc_module";
+</script>

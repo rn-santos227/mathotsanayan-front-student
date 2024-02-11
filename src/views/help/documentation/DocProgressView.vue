1432b48536a7c3fb8f765abc94d9ebbf3877a3b7
rn-santos227 <rn_santos227@yahoo.com>
2024-02-11 22:36:32 +0800

    update progress page in help panel

--- src/views/help/documentation/DocProgressView.vue
+++ src/views/help/documentation/DocProgressView.vue
@@ -1,14 +1,34 @@
 <template>
   <v-card-text>
-    <span class="text-h5 font-weight-bold">
-      MatHotSanayan Student Progress
-    </span>
-    <v-divider class="my-2" />
+    <div v-if="focusPage === 'progress-main'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Student Progress Table
+      </span>
+      <v-divider class="my-2" />
+      <p class="ma-6">
+        {{ docs[0] }}
+      </p>
+    </div>
+
+    <div v-if="focusPage === 'progress-details'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Student Progress Details
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'progress-answers'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Student Answers List
+      </span>
+      <v-divider class="my-2" />
+    </div>
   </v-card-text>
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import docs from "@/helpers/documentation/doc_progress";
 
 const props = defineProps<{
   page: string;

c8b7bc6f342a355fc46600b95a89eb201e317082
rn-santos227 <rn_santos227@yahoo.com>
2024-02-14 23:00:59 +0800

    update features page in help panel

--- src/views/help/documentation/DocFeatureView.vue
+++ src/views/help/documentation/DocFeatureView.vue
@@ -4,9 +4,27 @@
       MatHOTSanayan Student Features
     </span>
     <v-divider class="my-2" />
-    <p class="ma-6">
-      {{ docs[0] }}
-    </p>
+
+    <ul class="mx-10 my-4">
+      <li class="my-2">
+        <p>
+          <b>User-Friendly Interface:</b>
+          <span> {{ docs[1] }}</span>
+        </p>
+      </li>
+      <li class="my-2">
+        <p>
+          <b>Comprehensive Test Bank:</b>
+          <span> {{ docs[2] }}</span>
+        </p>
+      </li>
+      <li class="my-2">
+        <p>
+          <b>HOTS-focused Approach:</b>
+          <span> {{ docs[3] }}</span>
+        </p>
+      </li>
+    </ul>
   </v-card-text>
 </template>
 
